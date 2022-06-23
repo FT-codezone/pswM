98738fe9ecda169da3d79c8d71a2a778660e2d88
Federico Tosoni <FederacoPatalaco@gmail.com>
2022-06-23 14:00:18 +0200

    Aggiunta front-end alla pagina di registrazione

--- routes/register.js
+++ routes/register.js
@@ -5,28 +5,33 @@ const dbUrl = "mongodb://localhost:27017"
 const dbName = "pswM"
 
 router.get("/", (req,res)=>{
-    res.render("register",{idAlreadyUsed:false})
+    res.render("register",{idAlreadyUsed:false,campiOk:true})
 })
 
 router.post("/", (req,res)=>{
     MongoClient.connect(dbUrl, (err,database)=>{
         if(err) res.sendStatus(500)
         const db = database.db(dbName)
-        const {name,surname,id} = req.body
+        const {name,surname,email,id} = req.body
         db.collection("users").findOne({id:id}, (err,data)=>{
             if(err) res.sendStatus(500)
             if(data==null||data=={}||data==undefined){
-                db.collection("users").insertOne({
-                    id:id,
-                    name:name,
-                    surname:surname,
-                    firstAccess:true
-                })
-                req.session.logged = true
-                req.session.userId = id
-                res.redirect("http://localhost")
+                if(id!=null&&name!=null&&surname!=null&&email!=null){
+                    db.collection("users").insertOne({
+                        id:id,
+                        name:name,
+                        surname:surname,
+                        email:email,
+                        firstAccess:true
+                    })
+                    req.session.logged = true
+                    req.session.userId = id
+                    res.redirect("http://localhost")
+                }else{
+                    res.render("register",{idAlreadyUsed:false,campiOk:false})
+                }
             }else{
-                res.render("register",{idAlreadyUsed:true})
+                res.render("register",{idAlreadyUsed:true,campiOk:true})
             }
         })
     })
